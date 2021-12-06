c768ea49d4a43dd65c9e098ab53835f9d8542619
hoonkiyeo <hky1721@gmail.com>
2021-12-06 00:29:32 -0600

    modify chess_knight.py

--- implementation/chess_knight.py
+++ implementation/chess_knight.py
@@ -1,26 +1,20 @@
-import sys
-
 if __name__ == "__main__":
 
-    location = input()
-    if len(location) != 2:
-        print("wrong input")
+    loc = input()
+    row = int(loc[1])
+    col = int(ord(loc[0])) - int(ord("a")) + 1
 
-    dic = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
     steps = [(2,1), (2,-1), (-2,1), (-2,-1), (1,2), (-1,2), (1, -2), (-1,-2)]
-    x,y = int(dic[location[0]]), int(location[1])
     cnt = 0
 
     for step in steps:
-        x += step[0]
-        y += step[1]
-        if x >=1 and x <= 8 and y >=1 and y <= 8:
+        new_row = row + step[0]
+        new_col = col + step[1]
+        if new_row >=1 and new_row <= 8 and new_col >=1 and new_col <= 8:
             cnt += 1
-        x, y = int(dic[location[0]]), int(location[1])
         continue
 
     print(cnt)
-
 
 
 
