69a4e0950e116dc43ab3ec9a7befc6bed6f118df
hoonkiyeo <hky1721@gmail.com>
2022-01-19 01:19:30 -0600

    upload bfs_1.py

---
+++ dfs and bfs/bfs_1.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+n,m = map(int, input().split())
+arr = []
+
+for i in range(n):
+    arr.append(list(map(int, sys.stdin.readline().strip())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+
+    while q:
+        x,y = q.popleft()
+
+        for i in range(len(dx)):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if arr[nx][ny] == 0:
+                continue
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = arr[x][y] + 1
+                q.append((nx,ny))
+
+    return arr[n-1][m-1]
+
+print(bfs(0,0))
